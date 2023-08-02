12273cf894d52ca9eb07a517f56988aee2cc02b1
Kunal Parmar <kparmar@grepruby.io>
2023-08-02 18:41:10 +0530

    Chat APP

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const path = require("path");
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(path.resolve("./public")));
+
+const users = {};
+
+io.on("connection", (socket) => {
+  // console.log("A new user is connected", socket.id);
+  socket.on('new_user', (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit('name', name);
+  })
+
+
+//   socket.on("chat-message", (msg) => {
+//     console.log("a new user message", msg);
+//     // io.emit('new_message', msg);                    // send msg to every client/user including sender
+//     socket.broadcast.emit('new_message',msg);          //send msg to every client/user excluding sender
+//   });
+// });
+
+
+socket.on("chat-message", (msg) => {
+  console.log("a new user message", msg);
+  socket.broadcast.emit('new_message',{message: msg, name: users[socket.id]});          //send msg to every client/user excluding sender
+});
+});
+
+
+app.get("/", (req, res) => {
+  res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => {
+  console.log("server started at port : 9000");
+});
